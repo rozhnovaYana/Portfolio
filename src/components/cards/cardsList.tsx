0305736da696a763797c84a312eb7045789968bc
Yana Anatska <69202027+rozhnovaYana@users.noreply.github.com>
2024-02-26 17:32:59 +0200

    added Read More BTN

--- src/components/cards/cardsList.tsx
+++ src/components/cards/cardsList.tsx
@@ -1,36 +1,41 @@
 import Image from "next/image";
 import styles from "./cardsList.module.scss";
+import Link from "next/link";
+import { type ContentItem } from "@/helpers/markdown-reader";
 
-interface IItem {
-  id: string;
-  title: string;
-  description: string;
-  coverImage: string;
-}
 interface IItems {
   title: string;
-  fetchData: () => Promise<IItem[]>;
+  fetchData: () => ContentItem[];
 }
 
-export async function CardsList({ title, fetchData }: IItems) {
-  const items = await fetchData();
+export function CardsList({ title, fetchData }: IItems) {
+  const items = fetchData();
+  const titleSlug = title.toLowerCase();
 
   return (
     <div className={styles.card}>
       <h2 className={styles["card-title"]}>{title}</h2>
       <ul className={styles["card-items"]}>
         {items?.map((p) => (
-          <li key={p.id} className={styles.item}>
+          <li key={p.slug} className={styles.item}>
             <div className={styles["item-image"]}>
               <Image
                 sizes="(max-width: 768px) 50vw, 100vw"
                 fill
-                src={p.coverImage}
+                src={p?.coverImage}
                 alt={title}
               />
             </div>
-            <div className={styles["item-title"]}>{p.title}</div>
-            <div className={styles["item-description"]}>{p.description}</div>
+            <div className={styles["item-content"]}>
+              <div className={styles["item-title"]}>{p?.title}</div>
+              <div className={styles["item-description"]}>{p?.description}</div>
+            </div>
+            <Link
+              className={styles["item-button"]}
+              href={`${titleSlug}/${p.slug}`}
+            >
+              A Read More
+            </Link>
           </li>
         ))}
       </ul>
--- src/helpers/markdown-reader.ts
+++ src/helpers/markdown-reader.ts
@@ -1,15 +1,21 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+export type ContentItem = {
+  [key: string]: any;
+  content: string;
+  slug: string;
+};
 const rootDir = path.join(process.cwd(), "src", "data");
 
-export function getContent(file: string) {
+export function getContent(file: string): ContentItem[] {
   const dirName = path.join(rootDir, file);
   const fileNames = fs.readdirSync(dirName);
-  return fileNames.map((fileName: string) => {
+  return fileNames.map((fileName: string): ContentItem => {
     const filePath = path.join(dirName, fileName);
     const rawFile = fs.readFileSync(filePath, "utf8");
+    const slug = fileName.replace(/\.md$/, "");
     const { data, content } = matter(rawFile);
-    return { ...data, content };
+    return { ...data, content, slug };
   });
 }
